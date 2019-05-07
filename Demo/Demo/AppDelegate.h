5ddd0b33767fcc7032a66f27f52ae558de5c9927
谢齐 <941803686@qq.com>
2019-05-07 18:19:46 +0800

    创建版本0.0.1

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by wtwd on 2019/5/7.
+//  Copyright © 2019 wtwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by wtwd on 2019/5/7.
+//  Copyright © 2019 wtwd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
